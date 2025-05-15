e25310551278645d76469b8a7a32bef75dd0e1ba
ducnhann <since2005.ndn@gmail.com>
2025-05-15 10:50:37 +0700

    chore: Update missing sprites for screwblock and modify blocks' depth
    - Modified `Render` methods in `Bush.cpp` and `Ground.cpp` to include opacity parameters for sprite drawing.
    - Updated sprite frame names in `sprites.json` for consistency and clarity.
    - Standardized sprite names in `sprites.txt` to use hyphens instead of underscores.
    - Updated binary `sprites.png` to reflect changes in the sprite sheet.
    - Adjusted color values and depth settings in `world_1-1.json` to improve visual appearance and rendering order.

--- Ground.cpp
+++ Ground.cpp
@@ -27,25 +27,25 @@ void Ground::Render(DirectX::SpriteBatch* spriteBatch)
 
 	if(m_tileXcount == 1 && m_tileYcount == 1)
 	{
-        m_animator->Draw(spriteBatch, ID_SPRITE_INDEPENDENT_PLATFORM, pos);
+        m_animator->Draw(spriteBatch, ID_SPRITE_INDEPENDENT_PLATFORM, pos, 1.0f);
         return; // exit early if it's a single tile
     }
     // draw top left corner of the tile, then the middle, then the top right corner
-    m_animator->Draw(spriteBatch, ID_SPRITE_GROUND_TOP_LEFT, pos);
+    m_animator->Draw(spriteBatch, ID_SPRITE_GROUND_TOP_LEFT, pos, 1.0f);
     for (int i = 1; i < m_tileXcount - 1; i++)
     {
-        m_animator->Draw(spriteBatch, ID_SPRITE_GROUND_TOP_MID, pos + Vector2(i * tileSize.x, 0));
+        m_animator->Draw(spriteBatch, ID_SPRITE_GROUND_TOP_MID, pos + Vector2(i * tileSize.x, 0), 1.0f);
     }
-    m_animator->Draw(spriteBatch, ID_SPRITE_GROUND_TOP_RIGHT, pos + Vector2((m_tileXcount - 1) * tileSize.x, 0));
+    m_animator->Draw(spriteBatch, ID_SPRITE_GROUND_TOP_RIGHT, pos + Vector2((m_tileXcount - 1) * tileSize.x, 0), 1.0f);
     // draw the middle of the tile
     for (int j = 1; j < m_tileYcount; j++)
     {
-        m_animator->Draw(spriteBatch, ID_SPRITE_GROUND_BOT_LEFT, pos + Vector2(0, j * tileSize.y));
+        m_animator->Draw(spriteBatch, ID_SPRITE_GROUND_BOT_LEFT, pos + Vector2(0, j * tileSize.y), 1.0f);
         for (int i = 1; i < m_tileXcount - 1; i++)
         {
-            m_animator->Draw(spriteBatch, ID_SPRITE_GROUND_BOT_MID, pos + Vector2(i * tileSize.x, j * tileSize.y));
+            m_animator->Draw(spriteBatch, ID_SPRITE_GROUND_BOT_MID, pos + Vector2(i * tileSize.x, j * tileSize.y), 1.0f);
         }
-        m_animator->Draw(spriteBatch, ID_SPRITE_GROUND_BOT_RIGHT, pos + Vector2((m_tileXcount - 1) * tileSize.x, j * tileSize.y));
+        m_animator->Draw(spriteBatch, ID_SPRITE_GROUND_BOT_RIGHT, pos + Vector2((m_tileXcount - 1) * tileSize.x, j * tileSize.y), 1.0f);
     }
 }
 
